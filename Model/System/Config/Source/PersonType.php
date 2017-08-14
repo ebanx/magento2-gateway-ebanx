528727e8dd79c55e0ec1da9a9e5bbebaf715335d
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-08-14 10:26:22 -0300

    Create custom source to Person Type

---
+++ Model/System/Config/Source/PersonType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ebanx\PaymentGateway\Model\System\Config\Source;
+
+use Magento\Framework\Option\ArrayInterface;
+
+class PersonType implements ArrayInterface
+{
+    const CPF = "cpf";
+    const CNPJ = "cnpj";
+
+    public function toArray()
+    {
+        return [
+            self::CPF => __('CPF - Individuals'),
+            self::CNPJ => __('CNPJ - Companies')
+        ];
+    }
+
+    final public function toOptionArray()
+    {
+        $arr = $this->toArray();
+        $ret = [];
+
+        foreach ($arr as $key => $value) {
+            $ret[] = [
+                'value' => $key,
+                'label' => $value
+            ];
+        }
+
+        return $ret;
+    }
+}
