929c0791123bf75e95bffe8e73f82958a141d7dd
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-09-12 18:21:51 -0300

    Create Gateway files

---
+++ Model/Ui/ConfigProvider.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Ebanx\PaymentGateway\Model\Ui;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Ebanx\PaymentGateway\Gateway\Http\Client\ClientMock;
+
+/**
+ * Class ConfigProvider
+ */
+final class ConfigProvider implements ConfigProviderInterface
+{
+    const CODE = 'ebanx_boleto';
+
+    /**
+     * Retrieve assoc array of checkout configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [
+            'payment' => [
+                self::CODE => [
+                    'transactionResults' => [
+                        ClientMock::SUCCESS => __('Success'),
+                        ClientMock::FAILURE => __('Fraud')
+                    ]
+                ]
+            ]
+        ];
+    }
+}
