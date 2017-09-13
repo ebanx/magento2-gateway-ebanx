0e0c07889a1c50831dd3512c1f3adb6dcbb212fb
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-09-13 14:29:33 -0300

    Create Info block to order detail

---
+++ Block/Info.php
@@ -0,0 +1,31 @@
+<?php
+namespace Ebanx\PaymentGateway\Block;
+
+use Magento\Framework\Phrase;
+use Magento\Payment\Block\ConfigurableInfo;
+
+class Info extends ConfigurableInfo
+{
+    /**
+     * Returns label
+     *
+     * @param string $field
+     * @return Phrase
+     */
+    protected function getLabel($field)
+    {
+        return __($field);
+    }
+
+    /**
+     * Returns value view
+     *
+     * @param string $field
+     * @param string $value
+     * @return string | Phrase
+     */
+    protected function getValueView($field, $value)
+    {
+        return parent::getValueView($field, $value);
+    }
+}
