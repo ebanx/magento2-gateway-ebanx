2285b95c6eccd45d81740bb7232c7d3efa2f87f5
Guilherme Pressutto <gpressutto5@gmail.com>
2017-09-20 18:24:59 -0300

    View files boleto

---
+++ view/web/js/view/payment/ebanx-methods.js
@@ -0,0 +1,22 @@
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'uiComponent',
+        'Magento_Checkout/js/model/payment/renderer-list'
+    ],
+    function (
+        Component,
+        rendererList
+    ) {
+        'use strict';
+        rendererList.push(
+            {
+                type: 'ebanx_boleto',
+                component: 'Ebanx_Payments/js/view/payment/method-renderer/ebanx-boleto-method'
+            }
+        );
+        /** Add view logic here if needed */
+        return Component.extend({});
+    }
+);
