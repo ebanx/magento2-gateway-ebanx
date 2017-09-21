b89705110128992bf79a2e5c58807942ec86897a
Guilherme Coelho Piovesan <guilherme.coelho@ebanx.com>
2017-09-21 14:10:36 -0300

    Create method to get admin system values

--- Helper/Data.php
+++ Helper/Data.php
@@ -3,7 +3,32 @@
 namespace Ebanx\Payments\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Store\Model\ScopeInterface;
 
 class Data extends AbstractHelper
 {
+    /**
+     * @desc Retrieve payment values from admin configuration
+     * @param $field
+     * @param $paymentMethodCode
+     * @param $storeId
+     * @return mixed
+     */
+    public function getConfigData($field, $paymentMethodCode, $storeId)
+    {
+        $path = 'payment/' . $paymentMethodCode . '/' . $field;
+
+        return $this->scopeConfig->getValue($path, ScopeInterface::SCOPE_STORE, $storeId);
+    }
+
+    /**
+     * @desc Returns EBANX configuration values
+     * @param $field
+     * @param null $storeId
+     * @return mixed
+     */
+    public function getEbanxAbstractConfigData($field, $storeId = null)
+    {
+        return $this->getConfigData($field, 'ebanx_abstract', $storeId);
+    }
 }
