3a2ca514551fbf246173230afdb700aaa6ca433d
Guilherme Pressutto <gpressutto5@gmail.com>
2017-10-03 18:07:09 -0300

    Added benjamin to TransactionCapture

--- Gateway/Http/Client/TransactionCapture.php
+++ Gateway/Http/Client/TransactionCapture.php
@@ -13,6 +13,10 @@ use Ebanx\Payments\Logger\EbanxLogger;
  */
 class TransactionCapture implements ClientInterface
 {
+    /**
+     * @var \Ebanx\Benjamin\Facade
+     */
+    protected $_benjamin;
 
     /**
      * PaymentRequest constructor.
@@ -35,11 +39,8 @@ class TransactionCapture implements ClientInterface
         $this->_ebanxLogger = $ebanxLogger;
         $this->_appState = $context->getAppState();
 
-        // TODO: Connect Benjamin
-//        $benjamin = new \Ebanx\Benjamin();
-
-//        $this->_benjamin = $benjamin;
-
+        $api = new Api($this->_ebanxHelper);
+        $this->_benjamin = $api->benjamin();
     }
 
     /**
