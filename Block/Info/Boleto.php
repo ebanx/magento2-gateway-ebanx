172da5b97714ba04230b642b9940fabc3fbd9dae
Anderson Campanha <anderson.campanha@ebanx.com>
2017-11-08 14:16:51 -0200

    Fix order details page

--- Block/Info/Boleto.php
+++ Block/Info/Boleto.php
@@ -3,8 +3,4 @@ namespace Ebanx\Payments\Block\Info;
 
 class Boleto extends AbstractInfo
 {
-    /**
-     * @var string
-     */
-    protected $_template = 'Ebanx_Payments::info/ebanx_boleto.phtml';
 }
