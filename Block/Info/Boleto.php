c6ef47dc955b4f2dc8276e0b9acccc9b64e70b3e
Anderson Campanha <anderson.campanha@ebanx.com>
2017-11-08 15:22:09 -0200

    Add ebanx info on order detail

--- Block/Info/Boleto.php
+++ Block/Info/Boleto.php
@@ -3,4 +3,8 @@ namespace Ebanx\Payments\Block\Info;
 
 class Boleto extends AbstractInfo
 {
+    /**
+     * @var String
+     */
+    protected $_template = 'Ebanx_Payments::info/ebanx_boleto.phtml';
 }
