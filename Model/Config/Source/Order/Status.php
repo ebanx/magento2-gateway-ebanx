2e83f4e4b6d4d72b6b028c850c166fbf70f85901
Guilherme Pressutto <gpressutto5@gmail.com>
2017-11-28 19:41:55 -0200

    Returning real values on status source model

--- Model/Config/Source/Order/Status.php
+++ Model/Config/Source/Order/Status.php
@@ -2,20 +2,32 @@
 namespace Ebanx\Payments\Model\Config\Source\Order;
 
 use Magento\Framework\Option\ArrayInterface;
+use Magento\Sales\Model\ResourceModel\Order\Status\CollectionFactory;
 
 class Status implements ArrayInterface
 {
+    /**
+     * @var CollectionFactory
+     */
+    private $statusCollectionFactory;
+
+    /**
+     * Status constructor.
+     *
+     * @param CollectionFactory $statusCollectionFactory
+     */
+    public function __construct(CollectionFactory $statusCollectionFactory)
+    {
+        $this->statusCollectionFactory = $statusCollectionFactory;
+    }
+
     public function toOptionArray()
     {
-        return [
-            [
-                'value' => 'foo',
-                'label' => __('Foo'),
-            ],
-            [
-                'value' => 'bar',
-                'label' => __('Bar'),
-            ],
-        ];
+        return array_map(function($tag) {
+            return array(
+                'label' => $tag['label'],
+                'value' => $tag['status']
+            );
+        }, $this->statusCollectionFactory->create()->getData());
     }
 }
