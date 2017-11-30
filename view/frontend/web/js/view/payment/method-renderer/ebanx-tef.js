2f11177dbf0815de36837e29e40333b1bc4ece44
Anderson Campanha <anderson.campanha@ebanx.com>
2017-11-30 15:19:14 -0200

    Change success page class name to base

--- view/frontend/web/js/view/payment/method-renderer/ebanx-tef.js
+++ view/frontend/web/js/view/payment/method-renderer/ebanx-tef.js
@@ -2,13 +2,20 @@
 /*global define*/
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+        'mage/url'
     ],
-    function (Component) {
+    function (Component, urlBuilder) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Ebanx_Payments/payment/ebanx_tef'
+            },
+            afterPlaceOrder: function () {
+                window.location.href = urlBuilder.build('ebanx/tef/redirect');
+                this.placeOrder();
+
+                return false;
             }
         });
     }
