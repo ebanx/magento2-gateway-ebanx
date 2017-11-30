07256119c996ffc57ba8c2aa2c40a219bd3a7e2d
Rafael Bruno <rafaelbruno.web@gmail.com>
2017-11-30 15:59:12 -0200

    Fix height iframe

--- view/frontend/web/js/voucher.js
+++ view/frontend/web/js/voucher.js
@@ -2,7 +2,10 @@
     const loadIframe = () => {
         document.querySelector('#ebanx-boleto-frame').addEventListener("load", function() {
             const innerDoc = this.contentDocument || this.contentWindow.document;
-            const boletoHeight = (innerDoc.querySelector('.ebanx-boleto').clientHeight) - 500;
+            const customBlock = innerDoc.querySelector('.ebanx-boleto .custom');
+            const masterBlock = innerDoc.querySelector('.ebanx-boleto .mestre');
+            const boletoHeight = (customBlock.offsetHeight + masterBlock.offsetHeight) - this.offsetTop;
+            console.log(boletoHeight);
             const style = `
                 width: 100%; 
                 border: 0px;
