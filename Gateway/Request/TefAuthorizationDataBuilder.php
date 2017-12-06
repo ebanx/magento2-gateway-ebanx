4def708ba027bf73d653bf7a88e50c8cfd3d9d54
Anderson Campanha <anderson.campanha@ebanx.com>
2017-12-06 15:16:19 -0200

    Send selected bank to benjamin

--- Gateway/Request/TefAuthorizationDataBuilder.php
+++ Gateway/Request/TefAuthorizationDataBuilder.php
@@ -1,6 +1,8 @@
 <?php
 namespace Ebanx\Payments\Gateway\Request;
 
+use Ebanx\Payments\Observer\TefDataAssignObserver;
+use Magento\Payment\Gateway\Helper\SubjectReader;
 use Magento\Payment\Gateway\Request\BuilderInterface;
 
 class TefAuthorizationDataBuilder implements BuilderInterface
@@ -11,9 +13,14 @@ class TefAuthorizationDataBuilder implements BuilderInterface
      */
     public function build(array $buildSubject)
     {
+        /** @var \Magento\Payment\Gateway\Data\PaymentDataObject $paymentDataObject */
+        $paymentDataObject = SubjectReader::readPayment($buildSubject);
+        $payment = $paymentDataObject->getPayment();
+        $selectedBank = $payment->getAdditionalInformation(TefDataAssignObserver::SELECTED_BANK);
+
         return [
             'type' => 'tef',
-            'bankCode' => 'bradesco',
+            'bankCode' => $selectedBank,
         ];
     }
 }
