fc71a4a148f0b98b3778244ed55d80d2b7e7a8bf
Anderson Campanha <anderson.campanha@ebanx.com>
2017-12-15 17:05:48 -0200

    Validate that document field is filled

--- view/frontend/web/js/view/payment/method-renderer/ebanx-boleto.js
+++ view/frontend/web/js/view/payment/method-renderer/ebanx-boleto.js
@@ -2,14 +2,37 @@
 /*global define*/
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+        'jquery'
     ],
-    function (Component) {
+    function (Component, $) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Ebanx_Payments/payment/ebanx_boleto',
                 payment_document: null
+            },
+            getData: function () {
+                return {
+                    'method': this.getCode(),
+                    'additional_data': {
+                        'document': this.payment_document
+                    }
+                }
+            },
+            setCardData: function (data) {
+                this.payment_document = data.payment_document;
+            },
+            beforePlaceOrder: function (data) {
+                if (!this.validateForm('#document-form')) {
+                    return;
+                }
+
+                this.setCardData(data);
+                this.placeOrder();
+            },
+            validateForm: function (form) {
+                return $(form).validation() && $(form).validation('isValid');
             }
         });
     }
