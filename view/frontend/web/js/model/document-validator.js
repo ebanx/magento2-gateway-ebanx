4f5ab778552abf9c4fd8969a2df23a7976647c97
Anderson Campanha <anderson.campanha@ebanx.com>
2017-12-19 14:56:10 -0200

    Check if document is a valid cpf

--- view/frontend/web/js/model/document-validator.js
+++ view/frontend/web/js/model/document-validator.js
@@ -7,15 +7,59 @@ define(
     function ($) {
         'use strict';
         var interval = setInterval(function () {
-            if (typeof $.validator === 'undefined')
+            if (typeof $.validator === 'undefined'){
                 return;
+            }
 
             clearInterval(interval);
 
             $.validator.addMethod(
                 'document-validator',
-                function (document, item) {
-                    return false;
+                function (document) {
+                    document = document.replace(/[^\d]+/g,'');
+
+                    if(document === ''){
+                        return false;
+                    }
+
+                    if (document.length !== 11
+                    || document === "00000000000"
+                    || document === "11111111111"
+                    || document === "22222222222"
+                    || document === "33333333333"
+                    || document === "44444444444"
+                    || document === "55555555555"
+                    || document === "66666666666"
+                    || document === "77777777777"
+                    || document === "88888888888"
+                    || document === "99999999999"){
+                        return false;
+                    }
+
+                    var digitSum = 0;
+                    for (var i = 0; i < 9; i ++){
+                        digitSum += parseInt(document.charAt(i)) * (10 - i);
+                    }
+
+                    var firstVerifyingDigit = 11 - (digitSum % 11);
+                    if (firstVerifyingDigit === 10 || firstVerifyingDigit === 11){
+                        firstVerifyingDigit = 0;
+                    }
+
+                    if (firstVerifyingDigit !== parseInt(document.charAt(9))) {
+                        return false;
+                    }
+
+                    digitSum = 0;
+                    for (i = 0; i < 10; i ++){
+                        digitSum += parseInt(document.charAt(i)) * (11 - i);
+                    }
+
+                    var secondVerifyingDigit = 11 - (digitSum % 11);
+                    if (secondVerifyingDigit === 10 || secondVerifyingDigit === 11){
+                        secondVerifyingDigit = 0;
+                    }
+                    return secondVerifyingDigit === parseInt(document.charAt(10));
                 },
                 'CPF inválido'
             );
