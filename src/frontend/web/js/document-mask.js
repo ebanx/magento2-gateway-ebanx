13d92b631252d00b363e8f0d5285c1880be84951
Guilherme Pressutto <gpressutto5@gmail.com>
2017-12-21 21:01:26 -0200

    Requiring VMasker from node_modules

--- src/frontend/web/js/document-mask.js
+++ src/frontend/web/js/document-mask.js
@@ -1,6 +1,8 @@
 (function(){
+  const VMasker = require('vanilla-masker');
+
   console.log('caraca muleque');
-  document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener('DOMContentLoaded', function() {
     console.log('que dia que isso');
     console.log(document.querySelector('#ebanx_boleto_document'));
     VMasker(document.querySelector('#ebanx_boleto_document')).maskMoney();
