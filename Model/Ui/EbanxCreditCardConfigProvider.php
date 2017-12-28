ab18687c90e832533318f6ab894d8ae66d7d631e
Anderson Campanha <anderson.campanha@ebanx.com>
2017-12-28 14:30:26 -0200

    Get public integration key from admin for card tokenization

--- Model/Ui/EbanxCreditCardConfigProvider.php
+++ Model/Ui/EbanxCreditCardConfigProvider.php
@@ -1,18 +1,33 @@
 <?php
 namespace Ebanx\Payments\Model\Ui;
 
+use Ebanx\Payments\Helper\Data;
 use Magento\Checkout\Model\ConfigProviderInterface;
 
 class EbanxCreditCardConfigProvider implements ConfigProviderInterface
 {
 
     const CODE = 'ebanx_creditcard';
+    private $ebanxHelper;
+
+    public function __construct(Data $ebanxHelper) {
+        $this->ebanxHelper = $ebanxHelper;
+    }
 
     /**
      * @return array
      */
     public function getConfig()
     {
-        return [];
+        $mode = $this->ebanxHelper->getEbanxAbstractConfigData('mode') ? 'sandbox' : 'live';
+
+        return [
+            'payment' => [
+                'ebanx' => [
+                    'publicKey' => $this->ebanxHelper->getEbanxAbstractConfigData('integration_key_public_' . $mode),
+                    'debug' => $mode
+                ]
+            ]
+        ];
     }
 }
