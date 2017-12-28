b3e424351ebdbb3a71592bc83abf993abe1bd6a6
Anderson Campanha <anderson.campanha@ebanx.com>
2017-12-28 18:11:52 -0200

    Document on tef

--- view/frontend/web/js/view/payment/method-renderer/ebanx-tef.js
+++ view/frontend/web/js/view/payment/method-renderer/ebanx-tef.js
@@ -2,29 +2,48 @@
 /*global define*/
 define(
     [
-        'Magento_Checkout/js/view/payment/default'
+        'Magento_Checkout/js/view/payment/default',
+        'jquery',
+        'document-mask'
     ],
-    function (Component) {
+    function (Component, $, documentMask) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Ebanx_Payments/payment/ebanx_tef',
-                selectedBank: 'bradesco'
+                selectedBank: 'bradesco',
+                paymentDocument: window.checkoutConfig.payment.ebanx.customerDocument
+            },
+            initialize: function () {
+                this._super();
+                documentMask('#ebanx_tef_document');
             },
             getData: function() {
                 return {
                     'method': this.getCode(),
                     'additional_data': {
-                        'selected_bank': this.selectedBank
+                        'selected_bank': this.selectedBank,
+                        'document': this.paymentDocument
                     }
                 };
+            },
+            setDocument: function (paymentDocument) {
+                this.paymentDocument = paymentDocument;
             },
             setSelectedBank: function (selectedBank) {
                 this.selectedBank = selectedBank;
             },
             beforePlaceOrder: function (data) {
+                if (!this.validateForm('#' + this.getCode() + '_form')) {
+                    return;
+                }
+
                 this.setSelectedBank(data.selectedBank);
+                this.setDocument(data.paymentDocument);
                 this.placeOrder();
+            },
+            validateForm: function (form) {
+                return $(form).validation() && $(form).validation('isValid');
             }
         });
     }
