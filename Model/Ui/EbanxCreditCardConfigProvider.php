d218245191ff5696e1dc198a9b846da9035138bf
SparK-Cruz <spark.crz@gmail.com>
2017-12-28 15:49:28 -0200

    Abstracted document provider away from credit card

--- Model/Ui/EbanxCreditCardConfigProvider.php
+++ Model/Ui/EbanxCreditCardConfigProvider.php
@@ -1,33 +1,18 @@
 <?php
 namespace Ebanx\Payments\Model\Ui;
 
-use Ebanx\Payments\Helper\Data;
 use Magento\Checkout\Model\ConfigProviderInterface;
 
 class EbanxCreditCardConfigProvider implements ConfigProviderInterface
 {
 
     const CODE = 'ebanx_creditcard';
-    private $ebanxHelper;
-
-    public function __construct(Data $ebanxHelper) {
-        $this->ebanxHelper = $ebanxHelper;
-    }
 
     /**
      * @return array
      */
     public function getConfig()
     {
-        $mode = $this->ebanxHelper->getEbanxAbstractConfigData('mode') ? 'sandbox' : 'live';
-
-        return [
-            'payment' => [
-                'ebanx' => [
-                    'publicKey' => $this->ebanxHelper->getEbanxAbstractConfigData('integration_key_public_' . $mode),
-                    'debug' => $mode
-                ]
-            ]
-        ];
+        return [];
     }
 }
