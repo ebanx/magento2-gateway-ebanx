a709e6ca757e3375d55f0a4e7a382171dfc87b4a
Guilherme Pressutto <gpressutto5@gmail.com>
2018-01-10 11:30:02 -0200

    Refactored Data Assign Observer

--- Observer/SafetyPayDataAssignObserver.php
+++ Observer/SafetyPayDataAssignObserver.php
@@ -3,10 +3,9 @@
 namespace Ebanx\Payments\Observer;
 
 use Magento\Framework\Event\Observer;
-use Magento\Payment\Observer\AbstractDataAssignObserver;
 use Magento\Quote\Api\Data\PaymentInterface;
 
-class SafetyPayDataAssignObserver extends AbstractDataAssignObserver
+class SafetyPayDataAssignObserver extends BaseDataAssignObserver
 {
     const SAFETYPAY_TYPE = 'safetypay_type';
 
@@ -16,29 +15,4 @@ class SafetyPayDataAssignObserver extends AbstractDataAssignObserver
     protected $additionalInformationList = [
         self::SAFETYPAY_TYPE,
     ];
-
-    /**
-     * @param Observer $observer
-     * @return void
-     */
-    public function execute(Observer $observer)
-    {
-        $data = $this->readDataArgument($observer);
-
-        $additionalData = $data->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
-        if (!is_array($additionalData)) {
-            return;
-        }
-
-        $paymentInfo = $this->readPaymentModelArgument($observer);
-
-        foreach ($this->additionalInformationList as $additionalInformationKey) {
-            if (isset($additionalData[$additionalInformationKey])) {
-                $paymentInfo->setAdditionalInformation(
-                    $additionalInformationKey,
-                    $additionalData[$additionalInformationKey]
-                );
-            }
-        }
-    }
 }
