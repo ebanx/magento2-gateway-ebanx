44ac3553199032c87ab20060ffed437f77d28dc1
Guilherme Pressutto <gpressutto5@gmail.com>
2018-01-10 12:44:13 -0200

    Created SafetyPay Info Block and Thank You page

---
+++ Block/Info/SafetyPay.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ebanx\Payments\Block\Info;
+
+class SafetyPay extends AbstractInfo
+{
+    /**
+     * @var String
+     */
+    protected $_template = 'Ebanx_Payments::info/ebanx_safetypay.phtml';
+}
