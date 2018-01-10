1570781ef1f124f7b2a8c20f41097e46a3d098ed
Guilherme Pressutto <gpressutto5@gmail.com>
2018-01-10 11:12:16 -0200

    Created SafetyPay Authorization Data Builder

---
+++ Observer/SafetyPayDataAssignObserver.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ebanx\Payments\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Payment\Observer\AbstractDataAssignObserver;
+use Magento\Quote\Api\Data\PaymentInterface;
+
+class SafetyPayDataAssignObserver extends AbstractDataAssignObserver
+{
+    const SAFETYPAY_TYPE = 'safetypay_type';
+
+    /**
+     * @var array
+     */
+    protected $additionalInformationList = [
+        self::SAFETYPAY_TYPE,
+    ];
+
+    /**
+     * @param Observer $observer
+     * @return void
+     */
+    public function execute(Observer $observer)
+    {
+        $data = $this->readDataArgument($observer);
+
+        $additionalData = $data->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
+        if (!is_array($additionalData)) {
+            return;
+        }
+
+        $paymentInfo = $this->readPaymentModelArgument($observer);
+
+        foreach ($this->additionalInformationList as $additionalInformationKey) {
+            if (isset($additionalData[$additionalInformationKey])) {
+                $paymentInfo->setAdditionalInformation(
+                    $additionalInformationKey,
+                    $additionalData[$additionalInformationKey]
+                );
+            }
+        }
+    }
+}
