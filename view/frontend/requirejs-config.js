2c832cb6b7ec076322b98ad4dda51be7ae00a3d9
Guilherme Pressutto <gpressutto5@gmail.com>
2018-01-11 19:32:58 -0200

    Created PSE KO Component

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,7 @@
 var config = {
     paths: {
         'cc-br' : 'Ebanx_Payments/js/cc.min',
+        'eft' : 'Ebanx_Payments/js/eft.min',
         'card-js' : 'https://cdnjs.cloudflare.com/ajax/libs/card/2.4.0/card.min',
         'lib-js' : 'https://js.ebanx.com/ebanx-1.5.min',
         'document-mask': 'Ebanx_Payments/js/document-mask.min',
---
+++ view/frontend/web/js/view/payment/method-renderer/ebanx-eft.js
@@ -0,0 +1,54 @@
+/*browser:true*/
+/*global define*/
+/*global checkoutConfig*/
+define(
+    [
+        'Magento_Checkout/js/view/payment/default',
+        'jquery',
+        'eft',
+    ],
+    function (Component, $, eft) {
+        'use strict';
+        return Component.extend({
+            defaults: {
+                template: 'Ebanx_Payments/payment/ebanx_tef',
+                selectedBank: 'banco_agrario',
+                availableBanks: window.checkoutConfig.payment.ebanx.availableBanks
+            },
+            getData: function() {
+                return {
+                    'method': this.getCode(),
+                    'additional_data': {
+                        'selected_bank': this.selectedBank,
+                    }
+                };
+            },
+            initialize: function () {
+                this._super();
+                eft.populateBankSelectWithBanks('#bank-select-ebanx-eft', this.availableBanks);
+            },
+            setSelectedBank: function (selectedBank) {
+                this.selectedBank = selectedBank;
+            },
+            beforePlaceOrder: function (data) {
+                this.disableBtnPlaceOrder();
+                if (!this.validateForm('#' + this.getCode() + '_form')) {
+                    this.enableBtnPlaceOrder();
+                    return;
+                }
+
+                this.setSelectedBank(data.selectedBank);
+                this.placeOrder();
+            },
+            validateForm: function (form) {
+                return $(form).validation() && $(form).validation('isValid');
+            },
+            disableBtnPlaceOrder: function(){
+                $('#btn_tef_form_place_order').attr('disabled', 'disabled');
+            },
+            enableBtnPlaceOrder: function(){
+                $('#btn_tef_form_place_order').removeAttr('disabled');
+            }
+        });
+    }
+);
