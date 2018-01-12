b1e8c69fba4772428376d35caae4ae19289f94d2
Anderson Campanha <anderson.campanha@ebanx.com>
2018-01-12 17:44:50 -0200

    Fix typo on variable default

--- view/frontend/web/js/view/payment/method-renderer/ebanx-safetypay.js
+++ view/frontend/web/js/view/payment/method-renderer/ebanx-safetypay.js
@@ -10,7 +10,7 @@ define(
         return Component.extend({
             defaults: {
                 template: 'Ebanx_Payments/payment/ebanx_safetypay',
-                safetypayType: 'Online',
+                safetypayType: 'online',
             },
             getData: function() {
                 return {
