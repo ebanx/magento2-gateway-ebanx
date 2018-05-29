414cf52bbeca9ba6a3226a7e6a01d6b0a24d6c51
Anderson Campanha <anderson.campanha@ebanx.com>
2018-05-29 16:06:19 -0300

    Purge repo for new extension

--- Block/Success/Eft.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Ebanx\Payments\Block\Success;
-
-class Eft extends Redirect
-{
-}
--- src/frontend/web/js/redirect.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  const url = document.querySelector('#redirectURL').value;
-
-  setTimeout(() => {
-    window.location.replace(url);
-  }, 3000);
-})();
