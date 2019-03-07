5d78d3155b6f9755999135745cdbb75fc66b0f41
Andre Starosta <andre.starosta@ebanx.com>
2019-03-07 13:03:48 -0300

    Add fake test

--- Test/test.sh
+++ Test/test.sh
@@ -1,1 +1,7 @@
-exit 0
+#!/bin/bash
+
+[[ $TRAVIS_COMMIT_MESSAGE =~ ^(\[tests skip\]) ]] && echo "TESTS SKIP" && exit 0;
+
+cd $TRAVIS_BUILD_DIR
+cp .env.sample .env
+chmod +x $(pwd)/.scripts/install.sh && $(pwd)/.scripts/install.sh
