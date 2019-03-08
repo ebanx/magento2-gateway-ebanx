3e3769fde6eb9be6639a40fca31c7a206a2a3582
Andre Starosta <andre.starosta@ebanx.com>
2019-03-08 17:59:32 -0300

    Change directories architecture

--- Test/test.sh
+++ Test/test.sh
@@ -6,7 +6,7 @@ cd $TRAVIS_BUILD_DIR
 cp .env.sample .env
 chmod +x $(pwd)/.scripts/install.sh && $(pwd)/.scripts/install.sh
 
-cd $TRAVIS_BUILD_DIR/Test
+cd $TRAVIS_BUILD_DIR/Test/e2e
 
 npm ci
 npx cypress run --config video=false --project ./magento2 -s ./magento2/cypress/integration/sample_spec.js
