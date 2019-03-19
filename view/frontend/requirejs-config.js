42ffc7d71894fb4690337792100b186680d7be4e
Ruanito Santos <ruanito.santos@ebanx.com>
2019-03-19 10:13:41 -0300

    Update lib js version

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     map: {
         '*': {
-            'ebanx': 'https://js.ebanx.com/ebanx-1.5.min.js'
+            'ebanx': 'https://js.ebanx.com/ebanx-1.5.2.min.js'
         }
     }
 };
