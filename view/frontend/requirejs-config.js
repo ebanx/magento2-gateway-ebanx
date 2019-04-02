c3b11902b2baaaa4fd4d471da6007eae788b94c0
Andre Starosta <andre.starosta@ebanx.com>
2019-04-02 09:24:54 -0300

    Add Argentine DNI field, mask and validation

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,8 @@
 var config = {
     map: {
         '*': {
-            'ebanx': 'https://js.ebanx.com/ebanx-1.5.2.min.js'
+            'ebanx': 'https://js.ebanx.com/ebanx-1.5.2.min.js',
+            'jquery_mask': 'https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js',
         }
     }
 };
