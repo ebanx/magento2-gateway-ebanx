1d56b656d35c3cba118815655578829284fa3d8f
João Batista Neto <neto.joaobatista@gmail.com>
2021-05-07 18:38:51 -0300

    Add PIX as payment available to Brazil

---
+++ Block/Info/Brazil/Pix.php
@@ -0,0 +1,13 @@
+<?php
+namespace DigitalHub\Ebanx\Block\Info\Brazil;
+
+use DigitalHub\Ebanx\Block\Info\AbstractInfo;
+
+class Pix extends AbstractInfo
+{
+
+    /**
+     * @var string
+     */
+    protected $_template = 'DigitalHub_Ebanx::info/brazil/pix.phtml';
+}
---
+++ Gateway/Response/Brazil/Pix/CancelHandler.php
@@ -0,0 +1,34 @@
+<?php
+namespace DigitalHub\Ebanx\Gateway\Response\Brazil\Pix;
+
+use Magento\Payment\Gateway\Response\HandlerInterface;
+
+class CancelHandler implements HandlerInterface
+{
+    protected $_logger;
+
+    public function __construct(
+        \DigitalHub\Ebanx\Logger\Logger $logger
+    )
+    {
+        $this->_logger = $logger;
+
+        $this->_logger->info('CancelHandler :: __construct');
+    }
+
+    /**
+     * @param array $handlingSubject
+     * @param array $response
+     */
+    public function handle(array $handlingSubject, array $response)
+    {
+        $payment = \Magento\Payment\Gateway\Helper\SubjectReader::readPayment($handlingSubject);
+        $payment = $payment->getPayment();
+
+        $this->_logger->info('CancelHandler :: handle');
+
+        $payment->setIsTransactionPending(false);
+        $payment->setIsTransactionClosed(true);
+        $payment->setShouldCloseParentTransaction(true);
+    }
+}
